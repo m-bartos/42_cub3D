8f2bbfcb095c3d4abe6a67acd3d772dff26f8129
O.R <orezek@seznam.cz>
2024-06-21 11:30:50 +0200

    Proto: fix player movement

--- src/movement.c
+++ src/movement.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 14:59:53 by orezek            #+#    #+#             */
-/*   Updated: 2024/06/20 23:21:04 by orezek           ###   ########.fr       */
+/*   Updated: 2024/06/21 11:29:19 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,30 @@ void	move_player(mlx_key_data_t key, void *param)
 	// starting player coordinates
 	static double x = 600;
 	static double y = 600;
-	static double pdx;
-	static double pdy;
+	// delta x and y
+	double pdx;
+	double pdy;
+	// initial player angle
 	static int pa = 45;
 
-	int startX = 0;
-	int startY = 0;
 
-	int endX = 0;
-	int endY = 0;
+	double startX = 0.0;
+	double startY = 0.0;
 
+	double endX = 0;
+	double endY = 0;
+
+	// initial angle ??
 	pdx = cos(degToRad(pa));
 	pdy = sin(degToRad(pa));
+
+	startX = x;
+	startY = y;
+	endX = x+pdx*100;
+	endY = y-pdy*100;
+// color and planes
 	unsigned int color = get_rgba(0, 255, 0, 255);
-	//mlx_image_t *left_pane = (mlx_image_t *) param;
 	planes_t *game_planes = (planes_t *) param;
-
-	startX = round(x);
-	startY = round(y);
-	endX = round(x+pdx*100);
-	endY = round(y+pdy*100);
-
 ////////////////////////////////////////////////////////////////////////////////
 // Testing cleaning initial or previous screen - it works
 	int l_color = get_rgba(0, 10, 139, 255);
@@ -74,7 +77,7 @@ void	move_player(mlx_key_data_t key, void *param)
 	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 	};
 	draw_map(game_planes->left_pane, map);
-	draw_map(game_planes->right_pane, map);
+	//draw_map(game_planes->right_pane, map);
 ////////////////////////////////////////////////////////////////////////////////
 	if ((key.action == MLX_PRESS  || key.action == MLX_REPEAT) && key.key == MLX_KEY_D)
 		x += 5;
@@ -83,23 +86,23 @@ void	move_player(mlx_key_data_t key, void *param)
 	else if ((key.action == MLX_PRESS  || key.action == MLX_REPEAT) && key.key == MLX_KEY_W)
 	{
 		x += pdx * 5;
-		y += pdy * 5;
+		y -= pdy * 5;
 	}
 	else if ((key.action == MLX_PRESS  || key.action == MLX_REPEAT) && key.key == MLX_KEY_S)
 	{
 		x-= pdx * 5;
-		y-= pdy * 5;
+		y+= pdy * 5;
 	}
 	else if ((key.action == MLX_PRESS  || key.action == MLX_REPEAT) && key.key == MLX_KEY_LEFT)
 	{
-		pa -= 5;
+		pa += 5;
 		pa = FixAng(pa);
 		pdx = cos(degToRad(pa));
 		pdy = sin(degToRad(pa));
 	}
 	else if ((key.action == MLX_PRESS  || key.action == MLX_REPEAT) && key.key == MLX_KEY_RIGHT)
 	{
-		pa += 5;
+		pa -= 5;
 		pa = FixAng(pa);
 		pdx = cos(degToRad(pa));
 		pdy = sin(degToRad(pa));
@@ -110,55 +113,76 @@ void	move_player(mlx_key_data_t key, void *param)
 	}
 
 	// implement FOV
-	for (int r = 0; r < 90; r++)
+	for (int r = 0; r < 60; r++)
 	{
-		////////////////////////////////////////////////////////
-		// Ray casting rendering
-		// Ray Casting
+////////////////////////////////////////////////////////
+// Ray casting rendering
 		double h_distance = 0;
 		double v_distance = 0;
-		double fov = FixAng((pa - 90 / 2) + r);
-		printf("TEST:::%f\n", fov);
+		double fov = FixAng((pa - 60 / 2) + r);
+		//printf("TEST:::%f\n", fov);
 		point_t *hrc;
 		point_t *vrc;
 		player_location_t pl;
 		pl.player_angle = pa;
 		pl.player_angle = fov;
-		printf("Angle: %d\n", pl.player_angle);
+		//printf("Angle: %d\n", pl.player_angle);
 		pl.player_coordinates.x = startX;
 		pl.player_coordinates.y = startY;
 		hrc = get_horizontal_ray_coordinates(&pl, map);
 		vrc = get_vertical_ray_coordinates(&pl, map);
 
-		printf("Func - Horizontal: Rx: %f, Ry: %f\n", hrc->x,hrc->y);
-		printf("Func - Vertical: Rx: %f, Ry: %f\n", vrc->x, vrc->y);
+		//printf("Func - Horizontal: Rx: %f, Ry: %f\n", hrc->x,hrc->y);
+		//printf("Func - Vertical: Rx: %f, Ry: %f\n", vrc->x, vrc->y);
 
 		h_distance = sqrt((hrc->x - pl.player_coordinates.x) * (hrc->x - pl.player_coordinates.x) + (hrc->y - pl.player_coordinates.y) * (hrc->y - pl.player_coordinates.y));
 		v_distance = sqrt((vrc->x - pl.player_coordinates.x) * (vrc->x - pl.player_coordinates.x) + (vrc->y - pl.player_coordinates.y) * (vrc->y - pl.player_coordinates.y));
 
-		printf("Horizontal Distance: %f\n", h_distance);
-		printf("Vertical Distance: %f\n", v_distance);
+		//printf("Horizontal Distance: %f\n", h_distance);
+		//printf("Vertical Distance: %f\n", v_distance);
 		int ppp_color = get_rgba(0, 0, 0, 255);
 		if (v_distance < h_distance)
 		{
 			draw_line(game_planes->left_pane, pl.player_coordinates.x, pl.player_coordinates.y, vrc->x, vrc->y, ppp_color);
-			draw_line(game_planes->right_pane, pl.player_coordinates.x, pl.player_coordinates.y, vrc->x, vrc->y, ppp_color);
+			//draw_line(game_planes->right_pane, pl.player_coordinates.x, pl.player_coordinates.y, vrc->x, vrc->y, ppp_color);
 		}
 		else
 		{
 			draw_line(game_planes->left_pane, pl.player_coordinates.x, pl.player_coordinates.y, hrc->x, hrc->y, ppp_color);
-			draw_line(game_planes->right_pane, pl.player_coordinates.x, pl.player_coordinates.y, hrc->x, hrc->y, ppp_color);
+			//draw_line(game_planes->right_pane, pl.player_coordinates.x, pl.player_coordinates.y, hrc->x, hrc->y, ppp_color);
 		}
 	// End of Ray Casting
-		printf("R: %d\n", r);
+	/////////////////////////////////////////////////////////////////////
+	// Draw Wall
+		double corrected_distance;
+		if (v_distance < h_distance)
+			corrected_distance = v_distance * cos(degToRad(fov - pa));
+		else
+			corrected_distance = h_distance * cos(degToRad(fov - pa));
+
+		int screen_height = 1200;
+		int max_wall_height = screen_height; // Wall extends the whole vertical line when directly facing
+
+		// Calculate the wall height based on the distance
+		int line_height = (65 * max_wall_height) / corrected_distance;
+		if (line_height > screen_height) line_height = screen_height; // Ensure it doesn't exceed the screen height
+
+		int line_offset = (screen_height / 2) - (line_height / 2); // Centering the wall slice vertically
+
+		// Draw the wall slice by filling pixels vertically
+		for (int y = line_offset; y < line_offset + line_height; y++) {
+			mlx_put_pixel(game_planes->right_pane, r * 10 + 530, y, color);  // Scale the x-coordinate appropriately
+		}
 	}
 
 	/////////////////////////////////////////////////////////////////////
-	startX = round(x);
-	startY = round(y);
-	endX = round(x+pdx*100);
-	endY = round(y+pdy*100);
+	// Draw player line
+	printf("Player Angle: %d\n", pa);
+	startX = x;
+	startY = y;
+	endX = x+pdx*100;
+	endY = y-pdy*100;
 	//printf("End Points: PDX: %f PDY: %f\n", x+pdx, y+pdy);
 	draw_line(game_planes->left_pane, startX, startY, endX, endY, color);
-	draw_line(game_planes->right_pane, startX, startY, endX, endY, color);
+	//draw_line(game_planes->right_pane, startX, startY, endX, endY, color);
 }
