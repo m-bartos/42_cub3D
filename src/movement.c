66b711ea693c9668838bcc802df0c7c987962a0b
O.R <orezek@seznam.cz>
2024-06-24 12:19:48 +0200

    Proto: refactor movement.c

--- src/movement.c
+++ src/movement.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 14:59:53 by orezek            #+#    #+#             */
-/*   Updated: 2024/06/24 12:04:34 by orezek           ###   ########.fr       */
+/*   Updated: 2024/06/24 12:19:21 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,53 +25,15 @@ void	move_player(mlx_key_data_t key, void *param)
 	// initial player angle
 	static int pa = 45;
 
-
-	// double startX = 0.0;
-	// double startY = 0.0;
-
-	// double endX = 0;
-	// double endY = 0;
-
-	// initial angle
 	pdx = cos(deg_to_rad(pa));
 	pdy = sin(deg_to_rad(pa));
-
-// Ignore for now - used by Player only
-	// startX = x;
-	// startY = y;
-	// endX = x+pdx*100;
-	// endY = y-pdy*100;
-
-// planes
 	planes_t *game_planes = game->game_planes;
 
 ////////////////////////////////////////////////////////////////////////////////
-// Testing cleaning initial or previous screen - it works
+// Cleaning initial or previous screen
 
 	set_img_background(game_planes->left_plane, L_BACKGROUND);
 	set_img_background(game_planes->right_plane, R_BACKGROUND);
-	// int map[18][15] =
-	// {
-	// {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,1,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
-	// {1,0,0,1,1,1,1,0,0,0,0,0,0,0,1},
-	// {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	// {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	// };
-	// draw_map(game_planes->left_plane, map);
 	draw_map_char(game_planes->left_plane, game);
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -111,111 +73,13 @@ void	move_player(mlx_key_data_t key, void *param)
 	}
 	// implement FOV
 ////////////////////////////////////////////////////////
-// Ray casting rendering
+// Ray casting and Wall rendering
 
-	// Connect to the new game struct
+	// Connect key changes to the game struct
 	game->player.player_angle = pa;
 	game->player.player_coordinates.x = x;
 	game->player.player_coordinates.y = y;
 	draw_rays(game);
 	draw_player(game);
 	draw_wall(game);
-	// // for (int r = 0; r < 60; r++)
-	// // {
-	// // 	double h_distance = 0;
-	// // 	double v_distance = 0;
-	// // 	double fov = fix_ang((pa - 60 / 2) + r);
-
-	// // 	point_t *hrc;
-	// // 	point_t *vrc;
-	// // 	player_t pl;
-	// // 	pl.player_angle = pa;
-	// // 	pl.player_angle = fov;
-	// // 	//printf("Angle: %d\n", pl.player_angle);
-	// // 	pl.player_coordinates.x = startX;
-	// // 	pl.player_coordinates.y = startY;
-	// // 	// Here change to v1
-	// // 	// it consumes px, py, pa and map
-	// // 	hrc = get_horizontal_ray_coordinates(&pl, map);
-	// // 	vrc = get_vertical_ray_coordinates(&pl, map);
-
-
-	// // 	// hrc = get_horizontal_ray_coordinates_v1(game);
-	// // 	// vrc = get_vertical_ray_coordinates_v1(game);
-
-	// // 	//printf("Func - Horizontal: Rx: %f, Ry: %f\n", hrc->x,hrc->y);
-	// // 	//printf("Func - Vertical: Rx: %f, Ry: %f\n", vrc->x, vrc->y);
-
-	// // 	h_distance = sqrt((hrc->x - pl.player_coordinates.x) * (hrc->x - pl.player_coordinates.x) + (hrc->y - pl.player_coordinates.y) * (hrc->y - pl.player_coordinates.y));
-	// // 	v_distance = sqrt((vrc->x - pl.player_coordinates.x) * (vrc->x - pl.player_coordinates.x) + (vrc->y - pl.player_coordinates.y) * (vrc->y - pl.player_coordinates.y));
-	// // 	if (v_distance < h_distance)
-	// // 	{
-	// // 		draw_line(game_planes->left_plane, pl.player_coordinates.x, pl.player_coordinates.y, vrc->x, vrc->y, RAY);
-	// // 		//draw_line(game_planes->right_plane, pl.player_coordinates.x, pl.player_coordinates.y, vrc->x, vrc->y, color);
-	// // 	}
-	// // 	else
-	// // 	{
-	// // 		draw_line(game_planes->left_plane, pl.player_coordinates.x, pl.player_coordinates.y, hrc->x, hrc->y, RAY);
-	// // 		//draw_line(game_planes->right_plane, pl.player_coordinates.x, pl.player_coordinates.y, hrc->x, hrc->y, color);
-	// // 	}
-	// // End of Ray Casting
-	// /////////////////////////////////////////////////////////////////////
-	// // Draw Wall
-	// 	double corrected_distance;
-	// 	if (v_distance < h_distance)
-	// 		corrected_distance = v_distance * cos(deg_to_rad(fov - pa));
-	// 	else
-	// 		corrected_distance = h_distance * cos(deg_to_rad(fov - pa));
-
-	// 	int screen_height = game->game_planes->right_plane->height;
-	// 	int screen_width = game->game_planes->right_plane->width;
-	// 	int max_wall_height = screen_height; // Wall extends the whole vertical line when directly facing
-
-	// 	// Calculate the wall height based on the distance
-	// 	// Adjust the wall size multiplyer to something appropriate like 64 or 128
-	// 	int line_height = (64 * max_wall_height) / corrected_distance;
-	// 	if (line_height > screen_height)
-	// 		line_height = screen_height; // Ensure it doesn't exceed the screen height
-
-	// 	int line_offset = (screen_height / 2) - (line_height / 2); // Centering the wall slice vertically
-
-	// 	// Correct horizontal position for each ray
-	// 	int ray_x_position = (screen_width - 16) - r * (screen_width / 60); // Inverted to draw from left to right
-
-	// 	// Draw the wall slice by filling pixels vertically
-	// 	// for (int y = line_offset; y < line_offset + line_height; y++)
-	// 	//{
-	// 	// 	mlx_put_pixel(game_planes->right_plane, ray_x_position, y, color);  // Draw at the correct x-coordinate
-	// 	// }
-	// 	for (int i = 0; i < 16; i++)
-	// 	{
-	// 		// floor
-	// 		draw_line(game_planes->right_plane, ray_x_position, 1200, ray_x_position, line_offset, FLOOR);
-	// 		// wall
-	// 		draw_line(game_planes->right_plane, ray_x_position, line_offset, ray_x_position, line_offset + line_height, WALL);
-	// 		// ceiling
-	// 		draw_line(game_planes->right_plane, ray_x_position, 0, ray_x_position, line_offset, CEILING);
-	// 		//End of Drawing Walls
-	// 		ray_x_position += 1;
-	// 	}
-	// 	// // floor
-	// 	// draw_line(game_planes->right_plane, ray_x_position, 1200, ray_x_position, line_offset, f_color);
-	// 	// // wall
-	// 	// draw_line(game_planes->right_plane, ray_x_position, line_offset, ray_x_position, line_offset + line_height, color);
-	// 	// // ceiling
-	// 	// draw_line(game_planes->right_plane, ray_x_position, 0, ray_x_position, line_offset, c_color);
-	// 	//End of Drawing Walls
-	// }
-
-	// /////////////////////////////////////////////////////////////////////
-	// // Draw player line
-	// printf("Player Angle: %d\n", pa);
-	// printf("PDX: %f\n", pdx);
-	// printf("PDY: %f\n", pdy);
-	// startX = x;
-	// startY = y;
-	// endX = x+pdx*100;
-	// endY = y-pdy*100;
-	// draw_line(game_planes->left_plane, startX, startY, endX, endY, PLAYER);
-	// //draw_line(game_planes->right_plane, startX, startY, endX, endY, color);
 }
--- src/ray_cast_horizontal.c
+++ src/ray_cast_horizontal.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 12:14:09 by orezek            #+#    #+#             */
-/*   Updated: 2024/06/23 17:24:47 by orezek           ###   ########.fr       */
+/*   Updated: 2024/06/24 12:12:04 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,83 +15,83 @@
 // args: map, px, py, pa
 // returns: rx, ry
 
-point_t *get_horizontal_ray_coordinates(player_t *pl, int map[18][15])
-{
-	point_t *hor_ray;
-	hor_ray = malloc(sizeof(point_t));
-	if (!hor_ray)
-		return (NULL);
-	// Casting ray
-	// Temp map size
-	int map_x = 50;
-	int map_y = 50;
+// point_t *get_horizontal_ray_coordinates(player_t *pl, int map[18][15])
+// {
+// 	point_t *hor_ray;
+// 	hor_ray = malloc(sizeof(point_t));
+// 	if (!hor_ray)
+// 		return (NULL);
+// 	// Casting ray
+// 	// Temp map size
+// 	int map_x = 50;
+// 	int map_y = 50;
 
-	int pa = pl->player_angle; // Initial player angle and position
-	double px = pl->player_coordinates.x; //150
-	double py = pl->player_coordinates.y; // 400
-	// double pdx = cos(deg_to_rad(pa));
-	// double pdy = sin(deg_to_rad(pa));
+// 	int pa = pl->player_angle; // Initial player angle and position
+// 	double px = pl->player_coordinates.x; //150
+// 	double py = pl->player_coordinates.y; // 400
+// 	// double pdx = cos(deg_to_rad(pa));
+// 	// double pdy = sin(deg_to_rad(pa));
 
-	int r, mx, my, mp, dof;
-	double rx, ry, ra, xo, yo;
-	//double v_distance = 0;
-	// init the variables
-	mx = 0; my = 0; mp = 0; dof = 0;
-	rx = 0; ry = 0; ra = 0; xo = 0; yo = 0;
-	// ray angle = player angle
-	ra = deg_to_rad(pa);
+// 	int r, mx, my, mp, dof;
+// 	double rx, ry, ra, xo, yo;
+// 	//double v_distance = 0;
+// 	// init the variables
+// 	mx = 0; my = 0; mp = 0; dof = 0;
+// 	rx = 0; ry = 0; ra = 0; xo = 0; yo = 0;
+// 	// ray angle = player angle
+// 	ra = deg_to_rad(pa);
 
 
-	// Horizontal lines
-	// no of lines
-	for (r = 0; r < 1; r++)
-	{
-		ra = deg_to_rad(pa);
-		dof = 0;
-		float aTan = 1.0 / tan(ra);
-		// look up 0 - 180 right -> left right is 0
-		if (sin(ra) > 0.001)
-		{
-			ry = (((int) py >> 6) << 6) - 0.0001;
-			rx = (py - ry) * aTan + px;
-			yo = -64;
-			xo = -yo * aTan;
-		}
-		// look down 180 - 360
-		else if (sin(ra) < -0.001)
-		{
-			ry = (((int) py >> 6) << 6) + 64;
-			rx = (py - ry) * aTan+px;
-			yo = 64;
-			xo = -yo * aTan;
-		}
-		// look left or right - exactly horizontal
-		else
-		{
-			rx = 100000;
-			ry = 100000;
-			dof = 18;
-		}
-		while (dof < 18)
-		{
-			mx = (int) (rx)>>6;
-			my = (int) (ry)>>6;
-			mp = my * map_x + mx;
-			(void) mp;
-			if (mx >= 0 && mx < map_x && my >= 0 && my < map_y && map[my][mx] == 1)
-				dof = 18;
-			else
-			{
-				rx += xo;
-				ry += yo;
-				dof += 1;
-			}
-		}
-		hor_ray->x = rx;
-		hor_ray->y = ry;
-	}
-	return (hor_ray);
-}
+// 	// Horizontal lines
+// 	// no of lines
+// 	for (r = 0; r < 1; r++)
+// 	{
+// 		ra = deg_to_rad(pa);
+// 		dof = 0;
+// 		float aTan = 1.0 / tan(ra);
+// 		// look up 0 - 180 right -> left right is 0
+// 		if (sin(ra) > 0.001)
+// 		{
+// 			ry = (((int) py >> 6) << 6) - 0.0001;
+// 			rx = (py - ry) * aTan + px;
+// 			yo = -64;
+// 			xo = -yo * aTan;
+// 		}
+// 		// look down 180 - 360
+// 		else if (sin(ra) < -0.001)
+// 		{
+// 			ry = (((int) py >> 6) << 6) + 64;
+// 			rx = (py - ry) * aTan+px;
+// 			yo = 64;
+// 			xo = -yo * aTan;
+// 		}
+// 		// look left or right - exactly horizontal
+// 		else
+// 		{
+// 			rx = 100000;
+// 			ry = 100000;
+// 			dof = 18;
+// 		}
+// 		while (dof < 18)
+// 		{
+// 			mx = (int) (rx)>>6;
+// 			my = (int) (ry)>>6;
+// 			mp = my * map_x + mx;
+// 			(void) mp;
+// 			if (mx >= 0 && mx < map_x && my >= 0 && my < map_y && map[my][mx] == 1)
+// 				dof = 18;
+// 			else
+// 			{
+// 				rx += xo;
+// 				ry += yo;
+// 				dof += 1;
+// 			}
+// 		}
+// 		hor_ray->x = rx;
+// 		hor_ray->y = ry;
+// 	}
+// 	return (hor_ray);
+// }
 
 
 point_t	*get_horizontal_ray_coordinates_v1(game_t *game)
@@ -100,8 +100,6 @@ point_t	*get_horizontal_ray_coordinates_v1(game_t *game)
 	hor_ray = malloc(sizeof(point_t));
 	if (!hor_ray)
 		return (NULL);
-	// Casting ray
-	// Temp map size
 	int map_x = game->game_map.width;
 	int map_y = game->game_map.height;
 
@@ -109,13 +107,10 @@ point_t	*get_horizontal_ray_coordinates_v1(game_t *game)
 	double px = game->player.player_coordinates.x;
 	double py = game->player.player_coordinates.y;
 	char **map = game->game_map.map;
-	// double pdx = cos(deg_to_rad(pa));
-	// double pdy = sin(deg_to_rad(pa));
 
 	int r, mx, my, mp, dof;
 	double rx, ry, ra, xo, yo;
-	//double v_distance = 0;
-	// init the variables
+
 	mx = 0; my = 0; mp = 0; dof = 0;
 	rx = 0; ry = 0; ra = 0; xo = 0; yo = 0;
 	// ray angle = player angle
