a1c3a76ddb9d7e28cc2e64898fc952a907a1717f
m-bartos <m.bartos96@seznam.cz>
2024-06-27 21:10:37 +0200

    Update: main_parsing.c

--- main_parsing.c
+++ main_parsing.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 10:02:00 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/25 14:41:16 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/27 21:08:29 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ void	init_map(map_t *map)
 	map->width = 0;
 	map->map = NULL;
 	map->player = malloc(sizeof(player_t));
+	map->textures = malloc(sizeof(textures_t));
+	map->textures->angle_0 = NULL;
+	map->textures->angle_90 = NULL;
+	map->textures->angle_180 = NULL;
+	map->textures->angle_270 = NULL;
 	if (map->player == NULL)
 		exit(2);
 	map->square_size = 64;
@@ -35,5 +40,16 @@ int main (int argc, char **argv)
 	error_argc(argc);
 	init_map(&map);
 	get_map(&map, argv[1]);
+	free(map.player);
 	return (0);
 }
+
+
+// int	main ()
+// {
+// 	char **array;
+
+// 	array = ft_split_empty("\nah\n\n\naha\n\n", '\n');
+// 	// array = ft_split("ahaha", '\n');
+// 	ft_print_array(array);
+// }
