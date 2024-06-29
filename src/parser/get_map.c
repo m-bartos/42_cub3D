5c6313bb56913d7a566f62e8d7fa6a5d2e44898c
m-bartos <m.bartos96@seznam.cz>
2024-06-29 15:08:36 +0200

    Clean: clean and move funcs in get_map.c and line_utils.c

--- src/parser/get_map.c
+++ src/parser/get_map.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 10:31:44 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/29 14:40:39 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/29 15:07:57 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,6 @@ char	**get_file_array(int fd)
 	}
 	map_array = ft_split_empty(lines, '\n');
 	free(lines);
-	// ft_free_array(map_array); // test only
-	// clean_map(NULL); // test only
-	// exit(99); // test only
 	return (map_array);
 }
 
@@ -47,24 +44,20 @@ char	**file_to_array(char *map_name)
 {
 	int		fd;
 	char	**map_array;
-	// char	*map_path;
 
-	// map_path = ft_strjoin("map/", map_name);
-	// printf("Looking for map at: %s\n", map_path);
 	fd = open(map_name, O_RDONLY, 0444);
-	// free(map_path);
 	if (fd < 0)
 	{
-		ft_putstr_fd("Error: Map not found. It has to be in map folder.\n", 2); // change message
+		ft_putstr_fd("Error: Config file not found. \n", 2);
 		clean_map(NULL);
 		exit(1);
 	}
 	else
-		ft_putstr_fd("Map found!\n", 1);
+		ft_putstr_fd("Config file found!\n", 1);
 	map_array = get_file_array(fd);
 	if (map_array == NULL)
 	{
-		ft_putstr_fd("Error: Map file empty\n", 2);
+		ft_putstr_fd("Error: Config file empty\n", 2);
 		clean_map(NULL);
 		exit(2);
 	}
@@ -125,7 +118,7 @@ void	map_flood_fill(char **map_array, size_t y, size_t x)
 	{
 		ft_putstr_fd("Map error: Open map.\n", 2);
 		ft_free_array(map_array);
-		clean_map(NULL); //free array and map_t
+		clean_map(NULL);
 		exit (1);
 	}
 	map_flood_fill(map_array, y + 1, x);
@@ -136,14 +129,11 @@ void	map_flood_fill(char **map_array, size_t y, size_t x)
 
 char	**seperate_map(char **file_content)
 {
-	int	i;
-	int	first_line;
-	int	last_line;
+	int		i;
+	int		first_line;
+	int		last_line;
 	char	**map;
 
-	ft_putstr_fd("------FILE------\n", 1);
-	ft_print_array(file_content);
-	ft_putstr_fd("------FILE------\n\n", 1);
 	last_line = ft_len_of_arr(file_content) - 1;
 	while (last_line > 0)
 	{
@@ -169,47 +159,12 @@ char	**seperate_map(char **file_content)
 		file_content[first_line + i] = NULL;
 		i++;
 	}
-	ft_putstr_fd("---- MAP ----\n", 1);
-	ft_print_array(map);
-	ft_putstr_fd("---- MAP ----\n", 1);
-	ft_putstr_fd("\n", 1);
-	ft_putstr_fd("---- file_content ----\n", 1);
-	ft_print_array(file_content);
-	ft_putstr_fd("---- file_content ----\n\n", 1);
-	return(map);
-}
-
-char	*delete_extra_spaces(char *str)
-{
-	int		i;
-	char	*first_part;
-	char	*second_part;
-	char	*old_str;
-
-	if (str[0] == 0)
-		return (str);
-	i = 1;
-	while (str[i])
-	{
-		if (str[i] == ' ' && str[i - 1] == ' ')
-		{
-			old_str = str;
-			first_part = ft_substr_e(str, 0, i);
-			second_part = ft_substr_e(str, i + 1, ft_strlen(str));
-			str = ft_strjoin_e(first_part, second_part);
-			free(old_str);
-			free(first_part);
-			free(second_part);
-			i--;
-		}
-		i++;
-	}
-	return (str);
+	return (map);
 }
 
 void	replace_start_pos_in_map(char **map)
 {
-	int	i;
+	int		i;
 	size_t	j;
 
 	i = 0;
@@ -236,56 +191,25 @@ void	fill_map_struct(map_t *map, char *str)
 {
 	char	**map_flooded;
 
-	ft_putstr_fd("-------------------\n", 1);
-	ft_putstr_fd("-RUNNING_MAP_CHECK-\n", 1);
-	ft_putstr_fd("-------------------\n", 1);
-
 	check_suffix(str);
-
+	ft_putstr_fd("---RUNNING_CONFIG_FILE_CHECK----\n", 1);
 	map->temp_file_arr = file_to_array(str);
 	map->map = seperate_map(map->temp_file_arr);
-
 	get_textures(map, map->temp_file_arr);
 	check_textures(map);
 	get_colors(map, map->temp_file_arr);
 	check_colors(map);
-	ft_print_array(map->temp_file_arr);
 	ft_free_array(map->temp_file_arr);
 	map->temp_file_arr = NULL;
-
-	// printf("NO:/%s/, SO:/%s/, EA:/%s/, WE:/%s/\n", map->textures->angle_90, map->textures->angle_270, map->textures->angle_0, map->textures->angle_180);
-	// printf("Ceiling: %u, floor: %u\n", map->ceiling_color, map->floor_color);
-
-	// map checker:
-	// Error overwrites the map array with some old map
-	ft_print_array(map->map);
-	ft_putstr_fd("\n\n", 1);
 	map->map = add_borders_fill_spaces(map->map);
-	ft_print_array(map->map);
-	ft_putstr_fd("\n\n", 1);
-
 	check_start_possitions(map->map);
-	// check only one start possition
 	get_player_pos(map);
 	map_flooded = ft_arrdup(map->map);
-	ft_print_array(map_flooded);
 	map_flood_fill(map_flooded, map->player->coordinates.y, map->player->coordinates.x);
-
-	printf("-----FLOODED MAP-----\n"); //printing
-	ft_print_array(map_flooded); //printing
-	printf("-----FLOODED MAP-----\n\n");
-	ft_free_array(map_flooded);
-
-	ft_putstr_fd("-------------------\n", 1);
-	ft_putstr_fd("---MAP_CHECK_OK----\n", 1);
-	ft_putstr_fd("-------------------\n\n", 1);
-
+	ft_putstr_fd("---CONFIG_FILE_CHECK_OK----\n", 1);
 	replace_start_pos_in_map(map->map);
 	map->player->coordinates.x = map->player->coordinates.x * SQUARE_SIZE + SQUARE_SIZE / 2;
 	map->player->coordinates.y = map->player->coordinates.y * SQUARE_SIZE + SQUARE_SIZE / 2;
 	map->width = max_line_width(map->map);
 	map->height = ft_len_of_arr(map->map);
-	// printf("W:%d, H:%d\n", map->width, map->height);
-	// printf("Player angle:%f\n", map->player->player_angle);
 }
-
--- src/parser/line_utils.c
+++ src/parser/line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 14:33:34 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/29 14:34:16 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/29 15:03:47 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,32 @@ int	max_line_width(char **map_array)
 		i++;
 	}
 	return (max_length);
+}
+
+char	*delete_extra_spaces(char *str)
+{
+	int		i;
+	char	*first_part;
+	char	*second_part;
+	char	*old_str;
+
+	if (str[0] == 0)
+		return (str);
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i - 1] == ' ')
+		{
+			old_str = str;
+			first_part = ft_substr_e(str, 0, i);
+			second_part = ft_substr_e(str, i + 1, ft_strlen(str));
+			str = ft_strjoin_e(first_part, second_part);
+			free(old_str);
+			free(first_part);
+			free(second_part);
+			i--;
+		}
+		i++;
+	}
+	return (str);
 }
