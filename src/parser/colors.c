559a1488bca991f2f8af5428527042739adc9c7a
m-bartos <m.bartos96@seznam.cz>
2024-06-29 14:48:49 +0200

    Clean: colors.c

--- src/parser/colors.c
+++ src/parser/colors.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 14:31:45 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/29 14:32:58 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/29 14:47:07 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,15 @@ void	check_valid_color_line(char *str)
 	}
 }
 
-int get_color_from_str(char *str)
+void	invalid_colors(char **colors)
+{
+	ft_putstr_fd("Error: Not valid format of RGB color.\n", 2);
+	clean_map(NULL);
+	ft_free_array(colors);
+	exit(99);
+}
+
+int	get_color_from_str(char *str)
 {
 	char	**colors;
 	int		red;
@@ -41,24 +49,13 @@ int get_color_from_str(char *str)
 	check_valid_color_line(&str[2]);
 	colors = ft_split_e(&str[2], ',');
 	if (ft_len_of_arr(colors) < 3)
-	{
-		ft_putstr_fd("Error: Not valid format of RGB color.\n" , 2);
-		clean_map(NULL);
-		ft_free_array(colors);
-		exit(99);
-	}
+		invalid_colors(colors);
 	red = ft_atoi(colors[0]);
 	green = ft_atoi(colors[1]);
 	blue = ft_atoi(colors[2]);
-	printf("R:%u, G:%u, B:%u\n", red, green, blue);
-	if (red < 0 || green < 0 || blue < 0 ||
-		red > 255 || green > 255 || blue > 255)
-	{
-		ft_putstr_fd("Error: Not valid format of RGB color.\n" , 2);
-		clean_map(NULL);
-		ft_free_array(colors);
-		exit(99);
-	}
+	if (red < 0 || green < 0 || blue < 0
+		|| red > 255 || green > 255 || blue > 255)
+		invalid_colors(colors);
 	color = get_rgba(red, green, blue, 255);
 	ft_free_array(colors);
 	return (color);
@@ -71,7 +68,7 @@ void	get_colors(map_t *map, char **file_arr)
 	if (file_arr == NULL)
 		return ;
 	i = 0;
-	while(file_arr[i])
+	while (file_arr[i])
 	{
 		if (ft_strncmp(file_arr[i], "F ", 2) == 0)
 			map->floor_color = get_color_from_str(file_arr[i]);
@@ -85,7 +82,7 @@ void	check_colors(map_t *map)
 {
 	if (map->ceiling_color == 0 || map->floor_color == 0)
 	{
-		ft_putstr_fd("Error: Not all colors provided.\n", 2);
+		ft_putstr_fd("Error: Not all colors are provided.\n", 2);
 		clean_map(NULL);
 		exit(99);
 	}
