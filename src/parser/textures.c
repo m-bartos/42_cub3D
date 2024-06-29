7e02230c6550270cd7495a059cffa3b629e73c6f
m-bartos <m.bartos96@seznam.cz>
2024-06-29 17:09:05 +0200

    Clean: textures.c

--- src/parser/textures.c
+++ src/parser/textures.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 14:31:09 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/29 17:01:56 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/29 17:08:45 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,28 @@ mlx_texture_t	*load_png_from_path(char *path)
 	return (texture);
 }
 
-void	get_textures(map_t *map, char **file_arr)
+void	get_textures(map_t *map, char **file)
 {
 	int		i;
 	char	*temp_line;
 
-	if (file_arr == NULL)
+	if (file == NULL)
 		return ;
 	i = 0;
-	while (file_arr[i])
+	while (file[i])
 	{
-		temp_line = file_arr[i];
-		file_arr[i] = ft_strtrim_e(file_arr[i], " ");
+		temp_line = file[i];
+		file[i] = ft_strtrim_e(file[i], " ");
 		free(temp_line);
-		file_arr[i] = delete_extra_spaces(file_arr[i]);
-		if (ft_strncmp(file_arr[i], "NO ", 3) == 0 && !map->txts->angle_0)
-			map->txts->angle_0 = load_png_from_path(&file_arr[i][3]);
-		else if (ft_strncmp(file_arr[i], "SO ", 3) == 0 && !map->txts->angle_180)
-			map->txts->angle_180 = load_png_from_path(&file_arr[i][3]);
-		else if (ft_strncmp(file_arr[i], "EA ", 3) == 0 && !map->txts->angle_270)
-			map->txts->angle_270 = load_png_from_path(&file_arr[i][3]);
-		else if (ft_strncmp(file_arr[i], "WE ", 3) == 0 && !map->txts->angle_90)
-			map->txts->angle_90 = load_png_from_path(&file_arr[i][3]);
+		file[i] = delete_extra_spaces(file[i]);
+		if (ft_strncmp(file[i], "NO ", 3) == 0 && !map->txts->angle_0)
+			map->txts->angle_0 = load_png_from_path(&file[i][3]);
+		else if (ft_strncmp(file[i], "SO ", 3) == 0 && !map->txts->angle_180)
+			map->txts->angle_180 = load_png_from_path(&file[i][3]);
+		else if (ft_strncmp(file[i], "EA ", 3) == 0 && !map->txts->angle_270)
+			map->txts->angle_270 = load_png_from_path(&file[i][3]);
+		else if (ft_strncmp(file[i], "WE ", 3) == 0 && !map->txts->angle_90)
+			map->txts->angle_90 = load_png_from_path(&file[i][3]);
 		i++;
 	}
 }
