b27ef28c4f85efa081dccf8df6c036e4741736f7
m-bartos <m.bartos96@seznam.cz>
2024-06-29 14:48:43 +0200

    Clean: checkers.c

--- src/parser/checkers.c
+++ src/parser/checkers.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 14:34:44 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/29 14:35:23 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/29 14:43:20 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void	check_start_possitions(char **map_array)
 {
-	int	num_of_NSEW;
+	int	num_of_nsew;
 	int	i;
 	int	j;
 
 	i = 0;
-	num_of_NSEW = 0;
+	num_of_nsew = 0;
 	while (map_array[i])
 	{
 		j = 0;
-		while(j < (int) ft_strlen(map_array[i]))
+		while (j < (int) ft_strlen(map_array[i]))
 		{
 			if (map_array[i][j] == 'N' || map_array[i][j] == 'S'
 				|| map_array[i][j] == 'E' || map_array[i][j] == 'W')
-				num_of_NSEW += 1;
+				num_of_nsew += 1;
 			j++;
 		}
 		i++;
 	}
-	if (num_of_NSEW != 1)
+	if (num_of_nsew != 1)
 	{
 		ft_putstr_fd("Map error: More starts (NSEW).\n", 2);
 		clean_map(NULL);
